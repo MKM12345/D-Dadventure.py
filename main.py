902ea356bf6364e9b82599bd6b3293316f033573
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-02-23 21:26:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygzrun
+import pgzrun
 
 GRID_WIDTH = 16
 GRID_HEIGHT = 12
@@ -7,4 +7,16 @@ GRID_SIZE = 50
 WIDTH = GRID_WIDTH * GRID_SIZE
 HEIGHT = GRID_HEIGHT * GRID_SIZE
 
-pygzrun.go()
+pgzrun.go()
+
+HEIGHT = GRID_HEIGHT * GRID_SIZE
+def screen_chords:
+  return (x * GRID_SIZE, y * GRID_SIZE)
+def draw_background():
+  for y in range(GRID_HEIGHT):
+    for x in range(GRID_WIDTH):
+      screen.blit("floor1", screen_chords(x,y))
+      
+def draw():
+  draw_background()
+pgzrun.go()
