190d77b7f38c4bb20bcfc06c97961a883afcd7ed
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-02-24 18:42:29 +0000

    Created basic Functions

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ HEIGHT = GRID_HEIGHT * GRID_SIZE
 pgzrun.go()
 
 HEIGHT = GRID_HEIGHT * GRID_SIZE
-def screen_chords:
+def screen_coords:
   return (x * GRID_SIZE, y * GRID_SIZE)
 def draw_background():
   for y in range(GRID_HEIGHT):
@@ -35,9 +35,112 @@ MAP = ["WWWWWWWWWWWWWWWW",
        "W              W",
        "W              D",
        "WWWWWWWWWWWWWWWW",]
-            screen.blit("floor1", screen_chords(x,y))
+            screen.blit("floor1", screen_coords(x,y))
 def draw_scenery():
     for y in range(GRID_HEIGHT):
       for x in range(GRID_WIDTH):
         square = MAP[y] [x]
         if square == "W":
+          screen.blit("wall", screen_coords(x,y))
+        elif square == "D":
+          screen.blit("door", screen_coords(x,y))
+def draw():
+  draw_background()
+  draw_scenery()
+def screen_coords(x,y):
+  return (x * GRID_SIZE, y * GRID_SIZE)
+def setup_game():
+  global player
+  player = Actor("player",anchor=("left", "top"))
+      for y in range(GRID_HEIGHT):
+      for x in range(GRID_WIDTH):
+        square = MAP[y] [x]
+        if square == "P":
+          player.pos = screen_coordsx,y)
+# Draw the Player
+          screen.blit("door", screen_coords(x,y))
+def draw_actors():
+  player.draw()
+def draw():
+  draw_background()
+  draw_scenery()
+  draw_actors()
+setup_game()
+pgzrun.go()
+# Moving the Player
+      return (x * GRID_SIZE, y * GRID_SIZE)
+def grid_coords(actor):
+    return (round(actor.x / GRID_SIZE), round(actor.y / GRID_SIZE))
+  draw_actors()
+def on_key_down(key):
+  if key == keys.LEFT:
+    move_player(-1,0)
+  elif key == keys.UP:
+    move_player(0,-1)
+  elif key == keys.RIGHT:
+    move_player(1,0)
+  elif key == keys.DOWN:
+    move_player(0,1)
+def move_player(dx, dy):
+  (x,y) = grid_coords(player)
+  x += dx
+  y += dy
+  square = MAP[y] [x]
+          if square == "W":
+          return
+          elif square == "D":
+          return
+        player.pos = screen_coords(x, y)
+# Add the keys
+def setup_game():
+  global player, keys_to_collect
+  player = Actor("player",anchor=("left", "top"))
+  keys_to_collect = []
+      for y in range(GRID_HEIGHT):
+      for x in range(GRID_WIDTH):
+        square = MAP[y] [x]
+        if square == "P":
+          player.pos = screen_coords(x, y)
+        elif square == "K":
+          key = Actor("key", anchor=("left", "top"). \
+                      pos=screen_coords(x, y))
+          keys_to_collect.append(key)
+# Draw new key actors
+def draw_actors():
+  player.draw()
+  for keys in keys_to_collect:
+    key.draw()
+# Pick up the keys
+    elif square == "D":
+        if len(keys_to_collect) > 0:
+            return
+    for key in keys_to_collect:
+      (key_x, key_y) = grid_coords(key)
+      if x == key_x and y == key_y:
+        keys_to_colllect.remove(key)
+        break
+    player.pos = screen_coords(x, y)
+# Create the "Game Over!"
+def setup_game():
+  global game_over, player, keys_to_collect
+  game_over = False
+  player = Actor("player",anchor=("left", "top"))
+  keys_to_collect = []
+      for y in range(GRID_HEIGHT):
+def move_player (dx, dy):
+  global game_over
+  if game_over:
+    return
+  (x,y) = grid_coords(player)
+  x += dx
+  y += dy
+  square = MAP[y] [x]
+          if square == "W":
+          return
+          elif square == "D":
+          return 
+if len(keys_to_collect) > 0:
+  return
+else:
+  game_over = True
+for keys_in_keys_to_collect:
