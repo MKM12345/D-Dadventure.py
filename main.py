ad8c8a1c0da7655303f896a414a476af01713119
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-03-19 19:03:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,146 +1,95 @@
-import pgzrun
+import colorama
+from colorama import Fore    
+# GOOGLE SEARCH - https://www.studytonight.com/python-howtos/how-to-print-colored-text-in-python
 
-GRID_WIDTH = 16
-GRID_HEIGHT = 12
-GRID_SIZE = 50
+print(Fore.RED)              # RED COLOUR TEXT
+print(' ')                  # A line break
+print('****Welcome to Dungeons & Dragons****')
+print(' ')
+print("Your Dungon Master today will be me - Mantra Jhala")
+print(' ')
+print(Fore.WHITE) 
+print("You are standing on a pathway in the countryside with your guild.")
+print(' ')
+print("Suddenly, a trio of orcs rush you from a nearby bush! What do you do ?")
+print(' ')
+print(Fore.GREEN)            # GREEN COLOUR TEXT
+print("You have 2 choices. They are: 1 - Throw your hand axes at the orcs OR 2 - Send a spell at the orcs? : - ")
+print(' ')
+print(Fore.BLUE)             # BLUE COLOUR TEXT
 
-WIDTH = GRID_WIDTH * GRID_SIZE
-HEIGHT = GRID_HEIGHT * GRID_SIZE
+dnd_choice = input("Please enter either 1 or 2 as your choice here :- ")
+if dnd_choice == "1": # Run from bush 
+  # This code is indented 1 time because it is 1 choice deep
+  print(Fore.GREEN)
+  print(' ')
+  print("You threw hand axes however, the Orcs did not die and now Orcs are coming to attack you!")
+  print(' ')
+  print(Fore.BLUE)             # BLUE COLOUR TEXT
+  damage_choice = input("Do you 1. Disengage or 2. Tank the hit: - ")
 
-pgzrun.go()
+  if damage_choice == "1": # Powerful hit, die
+    # This code is indented 2 time because it is 2 choices deep
+    print(Fore.GREEN)            # GREEN COLOUR TEXT
+    print(' ')  
+    print("You disapper and appear again, 60 feet away.  The Orcs clumsily trip over each other and fall. You have disengaged, now you have 2 choices")
+    print(' ')
+    print(Fore.BLUE)             # BLUE COLOUR TEXT
+    disengage_choice = input("Do you 1. Run back into combat or 2. Let your friends have a shot and then run back in: - ")
 
-HEIGHT = GRID_HEIGHT * GRID_SIZE
-def screen_coords:
-  return (x * GRID_SIZE, y * GRID_SIZE)
-def draw_background():
-  for y in range(GRID_HEIGHT):
-    for x in range(GRID_WIDTH):
-      screen.blit("floor1", screen_chords(x,y))
+    if disengage_choice == "1": 
+    # This code is indented 3 time because it is 3 choices deep
+      print(Fore.GREEN)            # GREEN COLOUR TEXT
+      print(' ')      
+      print("The Orcs glare at you, when you run for the combat and strike you with their maul. You take too much damage & you Die")
+      print("GAME OVER")
       
-def draw():
-  draw_background()
-pgzrun.go()
-HEIGHT = GRID_HEIGHT * GRID_SIZE
-# Create the map
-MAP = ["WWWWWWWWWWWWWWWW",
-       "W              W",
-       "W              W",
-       "W  W  KG       W",
-       "W  WWWWWWWWWW  W",
-       "W              W",
-       "W      P       W",
-       "W              W",
-       "W  WWWWWWWWWW  W",
-       "W      GK   W  W",
-       "W              W",
-       "W              D",
-       "WWWWWWWWWWWWWWWW",]
-            screen.blit("floor1", screen_coords(x,y))
-def draw_scenery():
-    for y in range(GRID_HEIGHT):
-      for x in range(GRID_WIDTH):
-        square = MAP[y] [x]
-        if square == "W":
-          screen.blit("wall", screen_coords(x,y))
-        elif square == "D":
-          screen.blit("door", screen_coords(x,y))
-def draw():
-  draw_background()
-  draw_scenery()
-def screen_coords(x,y):
-  return (x * GRID_SIZE, y * GRID_SIZE)
-def setup_game():
-  global player
-  player = Actor("player",anchor=("left", "top"))
-      for y in range(GRID_HEIGHT):
-      for x in range(GRID_WIDTH):
-        square = MAP[y] [x]
-        if square == "P":
-          player.pos = screen_coordsx,y)
-# Draw the Player
-          screen.blit("door", screen_coords(x,y))
-def draw_actors():
-  player.draw()
-def draw():
-  draw_background()
-  draw_scenery()
-  draw_actors()
-setup_game()
-pgzrun.go()
-# Moving the Player
-      return (x * GRID_SIZE, y * GRID_SIZE)
-def grid_coords(actor):
-    return (round(actor.x / GRID_SIZE), round(actor.y / GRID_SIZE))
-  draw_actors()
-def on_key_down(key):
-  if key == keys.LEFT:
-    move_player(-1,0)
-  elif key == keys.UP:
-    move_player(0,-1)
-  elif key == keys.RIGHT:
-    move_player(1,0)
-  elif key == keys.DOWN:
-    move_player(0,1)
-def move_player(dx, dy):
-  (x,y) = grid_coords(player)
-  x += dx
-  y += dy
-  square = MAP[y] [x]
-          if square == "W":
-          return
-          elif square == "D":
-          return
-        player.pos = screen_coords(x, y)
-# Add the keys
-def setup_game():
-  global player, keys_to_collect
-  player = Actor("player",anchor=("left", "top"))
-  keys_to_collect = []
-      for y in range(GRID_HEIGHT):
-      for x in range(GRID_WIDTH):
-        square = MAP[y] [x]
-        if square == "P":
-          player.pos = screen_coords(x, y)
-        elif square == "K":
-          key = Actor("key", anchor=("left", "top"). \
-                      pos=screen_coords(x, y))
-          keys_to_collect.append(key)
-# Draw new key actors
-def draw_actors():
-  player.draw()
-  for keys in keys_to_collect:
-    key.draw()
-# Pick up the keys
-    elif square == "D":
-        if len(keys_to_collect) > 0:
-            return
-    for key in keys_to_collect:
-      (key_x, key_y) = grid_coords(key)
-      if x == key_x and y == key_y:
-        keys_to_colllect.remove(key)
-        break
-    player.pos = screen_coords(x, y)
-# Create the "Game Over!"
-def setup_game():
-  global game_over, player, keys_to_collect
-  game_over = False
-  player = Actor("player",anchor=("left", "top"))
-  keys_to_collect = []
-      for y in range(GRID_HEIGHT):
-def move_player (dx, dy):
-  global game_over
-  if game_over:
-    return
-  (x,y) = grid_coords(player)
-  x += dx
-  y += dy
-  square = MAP[y] [x]
-          if square == "W":
-          return
-          elif square == "D":
-          return 
-if len(keys_to_collect) > 0:
-  return
-else:
-  game_over = True
-for keys_in_keys_to_collect:
+    elif disengage_choice == "2":
+     # This code is indented 3 time because it is 3 choices deep 
+      print(Fore.GREEN)            # GREEN COLOUR TEXT
+      print(' ')
+      print("The Artificer's dragon, Kamiyami strikes the orcs with bolts of lightning and kills them.")
+      print("You see light through the trees and escape from the battle field, unhurt!")
+      print("YOU WIN")
+
+    elif disengage_choice != "1" or "2": # Check choices using logical operator (or) 
+      print(' ')
+      print("Incorrect input, please restart game")
+
+    
+  elif damage_choice == "2":
+  # This code is indented 2 time because it is returning back to 2nd level else if
+    print(Fore.GREEN)            # GREEN COLOUR TEXT
+    print(' ')
+    print("The Orcs simultaneously, smash you into a pancake with their mauls. You died. ")
+    print("GAME OVER")
+
+  elif damage_choice != "1" or "2": # Check choices using logical operator (or) 
+    print(' ')
+    print("Incorrect input, please restart game")
+   
+elif dnd_choice == "2":
+# This code is indented 1 time because it is returning back to 1st level else if
+  print(Fore.GREEN)            # GREEN COLOUR TEXT
+  print(' ')
+  print("You launch a kinetic blast so powerful it sends an orcs head hurtling away. The other 2 orcs are very angry, and so they smash Kamiyami into the floor. He is now on death saves. You have one potion of healing left. Do you either 1. Heal Kamiyami or 2. Only perform a medicine check on him.")
+  print(' ')
+  print(Fore.BLUE)             # BLUE COLOUR TEXT
+  healing_choice = input("Do you 1. Heal Kamiyami or 2. Only perform a medicine check on him: - ")
+  
+  if healing_choice == "1": 
+    print(' ')
+    print("GAME OVER - Kamiyami comes back to life and shoots bolts of lightning at the orcs and they die.  You Win.")
+    
+  elif healing_choice == "2":
+    print(' ')
+    print("GAME OVER - You tried to perform the medicine check, but you panic and poke the wound and make it worst. Kamiyami dies, orcs attack you, you take too much damage & die.")
+
+  elif healing_choice != "1" or "2": # Check choices using logical operator (or) 
+    print(' ')
+    print("Incorrect input, please restart game")
+
+elif dnd_choice != "1" or "2": # Check choices using logical operator (or) 
+  print(' ')
+  print("Incorrect input, please restart game")
+
